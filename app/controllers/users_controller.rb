d157b8d02cfe1d4304f9324855d5299e5e5906ff
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-01-26 18:48:48 -0500

    added routes to render json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
         user = User.find_by(id:params[:id]) 
         render json: user, include: [:brackets]
     end
+
+    def index  
+        users= User.all
+        render json: users, include: [:brackets]
+    end
+
+    def create 
+        user = User.find_or_create_by(email: user_params[:email])
+        render json: user, include: [:brackets]
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email)
+    end
 end
