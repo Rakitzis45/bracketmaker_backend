6a5dd3e8d7d688721e92144bd984d386b45f8197
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-01-27 20:01:10 -0500

    added controller actions create and update
    added strong params

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -3,4 +3,20 @@ class BracketsController < ApplicationController
         bracket = Bracket.find_by(id: params[:id]) 
         render json: bracket
     end
+
+    def create
+        bracket = Bracket.create(bracket_params)
+    end
+
+    def update
+        bracket = Bracket.find_by(id: params[:id])
+        bracket.update(bracket_params)
+    end
+
+    private 
+    def bracket_params
+        params.require(:bracket).permit(:id, :name, :position1, :position2, :position3, :position4, 
+            :position5, :position6, :position7, :position8, :code, :user_id, :total_teams, 
+            :position9, :position10, :position11, :position12, :position13, :position14, :position15)
+    end
 end
